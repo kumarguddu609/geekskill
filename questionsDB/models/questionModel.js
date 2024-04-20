6277d06ea318fa6722c2fe1e28ed589de891954b
Guddu Kumar <kumarguddu609@gmail.com>
2024-04-20 15:38:27 +0530

    adds db schema

---
+++ questionsDB/models/questionModel.js
@@ -0,0 +1,57 @@
+const pool = require("../db/db");
+
+// Create table if not exists
+pool
+  .query(
+    `
+  CREATE TABLE IF NOT EXISTS quiz_questions (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    description TEXT,
+    answers JSONB NOT NULL,
+    multiple_correct_answers BOOLEAN,
+    correct_answers JSONB NOT NULL,
+    explanation TEXT,
+    tags TEXT[],
+    category TEXT,
+    difficulty TEXT
+  );
+`
+  )
+  .catch((err) => console.error("Error creating table:", err));
+
+module.exports = {
+  createQuestion: async (questionData) => {
+    const {
+      question,
+      description,
+      answers,
+      multiple_correct_answers,
+      correct_answers,
+      explanation,
+      tags,
+      category,
+      difficulty,
+    } = questionData;
+
+    const { rows } = await pool.query(
+      `INSERT INTO quiz_questions 
+      (question, description, answers, multiple_correct_answers, correct_answers, explanation, tags, category, difficulty) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+      RETURNING id`,
+      [
+        question,
+        description,
+        answers,
+        multiple_correct_answers,
+        correct_answers,
+        explanation,
+        tags,
+        category,
+        difficulty,
+      ]
+    );
+
+    return rows[0].id;
+  },
+};
